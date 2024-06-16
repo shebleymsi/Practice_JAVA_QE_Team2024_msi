407c4f223fd37f8f290a512fbcf1465e6e843780
Shebleymsi <Shebley2022@gmail.com>
2024-06-16 11:36:24 -0400

    CheckAnagram update

---
+++ src/learnJavaQEWinter2022/problemsAndSolutionsPractice/CheckAnagram.java
@@ -0,0 +1,40 @@
+package learnJavaQEWinter2022.problemsAndSolutionsPractice;
+
+import java.util.Arrays;
+
+public class CheckAnagram {
+
+    public static void main(String[] args) {
+        String str1 = "Listen";
+        String str2 = "Silent";
+
+        if (areAnagrams(str1, str2)) {
+            System.out.println(str1 + " and " + str2 + " are anagrams.");
+        } else {
+            System.out.println(str1 + " and " + str2 + " are not anagrams.");
+        }
+    }
+
+    public static boolean areAnagrams(String str1, String str2) {
+        // Remove all whitespace and convert to lowercase
+        str1 = str1.replaceAll("\\s+", "").toLowerCase();
+        str2 = str2.replaceAll("\\s+", "").toLowerCase();
+
+        // Check if lengths are different
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        // Convert strings to char arrays
+        char[] charArray1 = str1.toCharArray();
+        char[] charArray2 = str2.toCharArray();
+
+        // Sort the char arrays
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+
+        // Compare the sorted char arrays
+        return Arrays.equals(charArray1, charArray2);
+    }
+
+}
