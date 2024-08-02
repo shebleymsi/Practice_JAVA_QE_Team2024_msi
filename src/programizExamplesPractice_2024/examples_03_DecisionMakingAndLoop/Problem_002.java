84962d86b125fa678d41696cd7ccf3e539292e40
Shebleymsi <Shebley2022@gmail.com>
2024-08-02 13:20:48 -0400

    Mid term : programiz update

--- src/programizExamplesPractice_2024/examples_03_DecisionMakingAndLoop/Problem_002.java
+++ src/programizExamplesPractice_2024/examples_03_DecisionMakingAndLoop/Problem_002.java
@@ -1,10 +1,21 @@
 package programizExamplesPractice_2024.examples_03_DecisionMakingAndLoop;
 
 
+import java.util.Scanner;
 
 public class Problem_002 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = input.nextInt();
 
-
-
+        if (number < 0) {
+            System.out.println("number is negative");
+        } else if (number > 0) {
+            System.out.println("number is positive");
+        }else {
+            System.out.println(" number is zero");
+        }
+    }
 
 }
