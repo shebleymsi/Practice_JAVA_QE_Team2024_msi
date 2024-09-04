1468f4be23b4ca683016b1b7e2be3949e0ff24f6
Shebleymsi <Shebley2022@gmail.com>
2024-09-03 21:28:55 -0400

    packag Kushal Parikh String update

---
+++ src/top_SDET_JAVA_Programs_Kushal_Parikh/sdet_03_arrays/SDET_01_Common_Elements_of_Two_Array.java
@@ -0,0 +1,34 @@
+package top_SDET_JAVA_Programs_Kushal_Parikh.sdet_03_arrays;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SDET_01_Common_Elements_of_Two_Array {
+    //Find common elements between  two arrays
+    //Input: array1 = {1,2,3,4,5} and  array2 = {4,5,6,7,8}
+    //Output: Common elements: [4, 5]
+
+    public static void main(String[] args) {
+        int[] array1 = {1, 2, 3, 4, 5};
+        int[] array2 = {4, 5, 6, 7, 8};
+        Set<Integer> commonElements = findCommonElements(array1, array2);
+        System.out.println("Common elements: " + commonElements);
+    }
+
+    public static Set<Integer> findCommonElements(int[] array1, int[] array2) {
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> commonSet = new HashSet<>();
+        // Add elements of the first array to the set
+        for (int num : array1) {
+            set1.add(num);
+        }
+        // Check for common elements in the second array
+        for (int num : array2) {
+            if (set1.contains(num)) {
+                commonSet.add(num);
+            }
+        }
+        return commonSet;
+    }
+
+}
