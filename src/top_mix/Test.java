5bae5047b255b4c4a5240e62225fa5a910ed0966
Shebleymsi <Shebley2022@gmail.com>
2024-10-31 05:00:53 -0400

    String immuteable

---
+++ src/top_mix/Test.java
@@ -0,0 +1,9 @@
+package top_mix;
+
+public class Test {
+    public static void main(String[] args) {
+        String str = "Hello";
+        str.concat(" World");
+        System.out.println(str);
+    }
+}
---
+++ src/top_w3schools/java_01_Tutorial/Main.java
@@ -0,0 +1,8 @@
+package top_w3schools.java_01_Tutorial;
+
+public class Main {
+    public static void main (String [] arg){
+        System.out.print("Hello world");
+
+    }
+}
