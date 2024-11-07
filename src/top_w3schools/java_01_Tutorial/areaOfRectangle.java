85a675b01fc7cc11c303ad3202f5ac58b10013b3
Shebleymsi <Shebley2022@gmail.com>
2024-11-07 14:15:22 -0500

    w3 Getting Started

---
+++ src/top_w3schools/java_01_Tutorial/areaOfRectangle.java
@@ -0,0 +1,15 @@
+package top_w3schools.java_01_Tutorial;
+
+public class areaOfRectangle {
+    public static void main(String[] args) {
+        int length = 10;
+        int width = 10;
+        int height = 10;
+
+        int area = length * width * height;
+        System.out.print(area);
+    }
+
+}
+
+//Boolean values are mostly used for conditional testing.
---
+++ src/top_w3schools/java_01_Tutorial/helloWorld.java
@@ -0,0 +1,21 @@
+package top_w3schools.java_01_Tutorial;
+
+public class helloWorld {
+    public static void main (String [] arg){
+        System.out.print("Hello worldHello world");
+    }
+}
+
+//The name of the java file must match the class name.
+
+//Any code inside the main() method will be executed.
+//just remember that every Java program has a class name which must match the filename,
+//and that every program must contain the main() method.
+//The curly braces {} marks the beginning and the end of a block of code.
+//System is a built-in Java class that contains useful members, such as out, which is short for "output".
+//The println() method, short for "print line", is used to print a value to the screen (or a file).
+//each code statement must end with a semicolon (;)
+
+
+//Java Variables continue ...
+
