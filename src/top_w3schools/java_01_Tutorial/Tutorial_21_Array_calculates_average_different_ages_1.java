6f627bbcefe5012c593f60c104ee8fe41e64a216
Shebleymsi <Shebley2022@gmail.com>
2024-11-21 06:45:00 -0500

    w3 Getting Started

--- src/top_w3schools/java_01_Tutorial/Tutorial_21_Array_calculates_average_different_ages_1.java
+++ src/top_w3schools/java_01_Tutorial/Tutorial_21_Array_calculates_average_different_ages_1.java
@@ -1,14 +1,14 @@
 package top_w3schools.java_01_Tutorial;
 
-public class Tutorial_21_Array_calculates_average_different_ages {
+public class Tutorial_21_Array_calculates_average_different_ages_1 {
     public static void main(String[] args) {
 
         int ages[] = {20, 22, 18, 35, 48, 26, 87, 70};
 
         float avg, sum = 0;
 
-        for (int age : ages) {
-            sum = sum + age; //sum +=;
+        for (int i = 0; i < ages.length; i++) {
+            sum  = sum + ages[i];
         }
         System.out.println(sum);
 
