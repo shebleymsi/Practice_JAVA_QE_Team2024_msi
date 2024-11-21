ac066acc9527178d13cfe69afb173f5ce80558b5
Shebleymsi <Shebley2022@gmail.com>
2024-11-21 05:25:05 -0500

    String immuteable

---
+++ src/top_w3schools/java_01_Tutorial/Tutorial_21_Array_calculates_average_different_ages.java
@@ -0,0 +1,19 @@
+package top_w3schools.java_01_Tutorial;
+
+public class Tutorial_21_Array_calculates_average_different_ages {
+    public static void main(String[] args) {
+
+        int ages[] = {20, 22, 18, 35, 48, 26, 87, 70};
+        int length = ages.length;
+
+        float avg, sum = 0;
+        for (int age : ages) {
+            sum = sum + age; //sum +=;
+        }
+        avg = sum / length;
+
+        System.out.println("The average age is: " + avg);
+    }
+}
+
+//let's create a program that calculates the average of different ages:
