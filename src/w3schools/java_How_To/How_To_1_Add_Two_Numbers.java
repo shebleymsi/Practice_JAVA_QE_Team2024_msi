5f2d92fa1d373b95b2b992ed7f559847139fc9e2
SHEBLEYMSI <shebleymsi@gmail.com>
2024-12-05 02:09:35 -0500

    IDEA setup test

---
+++ src/w3schools/java_How_To/How_To_1_Add_Two_Numbers.java
@@ -0,0 +1,12 @@
+package w3schools.java_How_To;
+
+public class How_To_1_Add_Two_Numbers {
+
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int sum = num1 + num2;
+        System.out.println(sum);
+    }
+
+}
---
+++ src/w3schools/java_How_To/How_To_1_Add_Two_Numbers_User_Input.java
@@ -0,0 +1,23 @@
+package w3schools.java_How_To;
+
+import java.util.Scanner;
+
+public class How_To_1_Add_Two_Numbers_User_Input {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter First number: ");
+        int num1 = input.nextInt();
+
+        System.out.println("Enter second number: ");
+        int num2 = input.nextInt();
+
+        int sum = num1 + num2;
+        System.out.println("Sum is: " +sum);
+
+        input.close();
+
+
+    }
+
+}
