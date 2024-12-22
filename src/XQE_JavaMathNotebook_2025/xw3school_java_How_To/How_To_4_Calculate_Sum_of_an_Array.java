5c6bae1d91b09339748bcf61eb72e5ebf3cf5788
SHEBLEYMSI <shebleymsi@gmail.com>
2024-12-22 13:53:16 -0500

    practice note book : Array lowest number

---
+++ src/XQE_JavaMathNotebook_2025/xw3school_java_How_To/How_To_4_Calculate_Sum_of_an_Array.java
@@ -0,0 +1,12 @@
+package XQE_JavaMathNotebook_2025.xw3school_java_How_To;
+
+public class How_To_4_Calculate_Sum_of_an_Array {
+    public static void main(String[] args) {
+        int[] myArray = {10, 20, 30, 40, 50};
+        int sum = 0;
+        for (int i = 0; i < myArray.length; i++) {
+            sum = sum + myArray[i];
+        }
+        System.out.println(sum);
+    }
+}
---
+++ src/XQE_JavaMathNotebook_2025/xw3school_java_How_To/How_To_6_Sort_an_Array.java
@@ -0,0 +1,14 @@
+package XQE_JavaMathNotebook_2025.xw3school_java_How_To;
+
+import java.util.Arrays;
+
+public class How_To_6_Sort_an_Array {
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW", "Tesla", "Ford", "Fiat", "Mazda", "Audi"};
+        Arrays.sort(cars);
+        for (String car : cars) {
+            System.out.println(car);
+        }
+    }
+}
+//You can use the sort() method, found in java.util.Arrays, to sort an array:
